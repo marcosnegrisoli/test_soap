fbd11f841c2a154a0e2dc79628562aa98a547664
Marcos Negrisoli <marcos@negrisoli.net>
2023-10-24 15:03:26 -0300

    Add project files.

---
+++ test/App_Code/service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://www.summitlink.net/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+public class service : System.Web.Services.WebService
+{
+    public class QuoteRequest
+    {
+        public String name { get; set; }
+    }
+
+    public class QuoteResponse
+    {
+        public Decimal value { get; set; }
+    }
+
+
+    public service()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public QuoteResponse getQuote(QuoteRequest request)
+    {
+        return new QuoteResponse() { value = 0 };
+    }
+
+}
